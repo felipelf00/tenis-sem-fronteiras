84e285e396d42302a8a4cc571025891931db25f6
felipewantstocode <118938786+felipewantstocode@users.noreply.github.com>
2024-01-15 22:25:07 -0300

    fixed burger menu

--- src/Navigation.tsx
+++ src/Navigation.tsx
@@ -1,14 +1,38 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import NavLinks from "./NavLinks";
-import { Twirl as Hamburger } from "hamburger-react";
+// import { Twirl as Hamburger } from "hamburger-react";
+import Burger from "./Burger";
+import { useClickAway } from "react-use";
 
 const Navigation = () => {
   const [isOpen, setIsOpen] = useState(false);
   const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
 
-  // const toggleMenu = () => {
-  //   setIsOpen(!isOpen);
-  // };
+  const navRef = useRef<HTMLUListElement | null>(null);
+  const hamburgerRef = useRef<HTMLButtonElement | null>(null);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  // useClickAway(navRef, (e) => {
+  //   if (
+  //     !navRef.current?.contains(e.target as Node) &&
+  //     !hamburgerRef.current?.contains(e.target as Node)
+  //   ) {
+  //     setIsOpen(false);
+  //   }
+  // });
+  useClickAway(navRef, (e): void => {
+    const clickedOutsideNav = !navRef.current?.contains(e.target as Node);
+
+    console.log(hamburgerRef);
+
+    const clickedOnHamburger = hamburgerRef.current?.contains(e.target as Node);
+    if (clickedOutsideNav && !clickedOnHamburger) {
+      setIsOpen(false);
+    }
+  });
 
   useEffect(() => {
     const handleResize = () => {
@@ -23,44 +47,9 @@ const Navigation = () => {
   //prettier-ignore
   return (
     <div>
-      {/* <button
-        type="button"
-        className="text-white md:hidden"
-        onClick={toggleMenu}
-      >
-        <svg
-          className="h-6 w-6 fill-current"
-          xmlns="http://www.w3.org/2000/svg"
-          x="0px"
-          y="0px"
-          width="100"
-          height="100"
-          viewBox="0,0,256,256"
-        >
-          <g
-            fillRule="nonzero"
-            stroke="none"
-            strokeWidth="1"
-            strokeLinecap="butt"
-            strokeLinejoin="miter"
-            strokeMiterlimit="10"
-            strokeDasharray=""
-            strokeDashoffset="0"
-            fontFamily="none"
-            fontWeight="none"
-            fontSize="none"
-            textAnchor="none"
-            style={{ mixBlendMode: "normal" }}
-          >
-            <g transform="scale(5.12,5.12)">
-              <path d="M5,8c-0.72127,-0.0102 -1.39216,0.36875 -1.75578,0.99175c-0.36361,0.623 -0.36361,1.39351 0,2.01651c0.36361,0.623 1.0345,1.00195 1.75578,0.99175h40c0.72127,0.0102 1.39216,-0.36875 1.75578,-0.99175c0.36361,-0.623 0.36361,-1.39351 0,-2.01651c-0.36361,-0.623 -1.0345,-1.00195 -1.75578,-0.99175zM5,23c-0.72127,-0.0102 -1.39216,0.36875 -1.75578,0.99175c-0.36361,0.623 -0.36361,1.39351 0,2.01651c0.36361,0.623 1.0345,1.00195 1.75578,0.99175h40c0.72127,0.0102 1.39216,-0.36875 1.75578,-0.99175c0.36361,-0.623 0.36361,-1.39351 0,-2.01651c-0.36361,-0.623 -1.0345,-1.00195 -1.75578,-0.99175zM5,38c-0.72127,-0.0102 -1.39216,0.36875 -1.75578,0.99175c-0.36361,0.623 -0.36361,1.39351 0,2.01651c0.36361,0.623 1.0345,1.00195 1.75578,0.99175h40c0.72127,0.0102 1.39216,-0.36875 1.75578,-0.99175c0.36361,-0.623 0.36361,-1.39351 0,-2.01651c-0.36361,-0.623 -1.0345,-1.00195 -1.75578,-0.99175z"></path>
-            </g>
-          </g>
-        </svg>
-      </button> */}
-      <Hamburger size={20} color={"white"} toggled={isOpen} toggle={setIsOpen}/>
+      {isMobile && <Burger ref={hamburgerRef} toggleMenu={toggleMenu}/>}
 
-      {isMobile ? (isOpen && <NavLinks />) : <NavLinks />}
+      {isMobile ? (isOpen && <NavLinks ref={navRef} />) : <NavLinks />}
     </div>
   );
 };
